5682309bdf695323f888534ad34193b38904d48c
coderhyp <slimshadyedc@gmail.com>
2023-02-17 22:43:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import DropdownTreeSelect from 'react-dropdown-tree-select'
+import 'react-dropdown-tree-select/dist/styles.css'
+
+const data = {
+  label: 'search me',
+  value: 'searchme',
+  children: [
+    {
+      label: 'search me too',
+      value: 'searchmetoo',
+      children: [
+        {
+          label: 'No one can get me',
+          value: 'anonymous',
+        },
+      ],
+    },
+  ],
+}
+
+const onChange = (currentNode, selectedNodes) => {
+  console.log('onChange::', currentNode, selectedNodes)
+}
+const onAction = (node, action) => {
+  console.log('onAction::', action, node)
+}
+const onNodeToggle = currentNode => {
+  console.log('onNodeToggle::', currentNode)
+}
+
+function App() {
+  return (
+    <div className="App">
+      <DropdownTreeSelect data={data} onChange={onChange} onAction={onAction} onNodeToggle={onNodeToggle} />
+    </div>
+  );
+}
+
+export default App;
